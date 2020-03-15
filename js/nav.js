f2ccf9259eb787037c99c5432edee42c99aab808
bugman001 <jhlee@bitnine.net>
2020-03-15 23:23:36 +0900

    기획자료추가 및 메인화면 수치값 vw,vh로 변경

--- js/nav.js
+++ js/nav.js
@@ -5,13 +5,21 @@
     // Scroll Down 
     if (scrollPercent >= 2) {
         $('#webUI').addClass('show');
-        $('.navbar-header.desk .logo img').attr('src','images/logo.png')
+        $('.navbar-header.desk .logo img').attr('src','./images/logo.png'); 
+
+        $('#insta').attr('src','./images/icon/insta-w.svg'); 
+        $('#facebook').attr('src','./images/icon/facebook-w.svg'); 
+        $('#git').attr('src','./images/icon/github-w.svg'); 
         
     } else {
         // Scroll Up
         if (scrollPercent <= 2) {
             $('#webUI').removeClass('show');
             $('.navbar-header.desk .logo img').attr('src','images/logo2.png')
+
+            $('#insta').attr('src','./images/icon/insta.svg'); 
+            $('#facebook').attr('src','./images/icon/facebook.svg'); 
+            $('#git').attr('src','./images/icon/github.svg'); 
         }
     }
 });
