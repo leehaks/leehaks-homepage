b39b9414ba5f50588369fd07fe00e950f54a2cf2
junghak <jhlee@bitnine.net>
2020-03-25 14:42:43 +0900

    scroll pageYOffset 에 따른 menu 링크효과 변화

--- js/app.js
+++ js/app.js
@@ -61,6 +61,18 @@ window.onload = function(){
         tabComponent[i].addEventListener('click', tabHandler); 
     }
 
+
+    // %값에 따른 progressbar 컬러값 채우기
+
+    const progress = document.querySelectorAll('.progress-bar'); 
+
+    for(let i=0;i<progress.length;i++){
+        let progressWidth = progress[i].getAttribute('data-num'); 
+        progress[i].querySelector('.progress').style.width = progressWidth + "%"
+    }
+
+    // 모바일용 메뉴 애니메이션
+
     const navToggleBtn = document.querySelector('.navbar-toggle-btn'); 
     const navMenu = document.querySelector('.navbar-menu'); 
     const navLink = document.querySelector('.navbar-link'); 
@@ -71,11 +83,61 @@ window.onload = function(){
         navLink.classList.toggle('active'); 
     })
 
-    const progress = document.querySelectorAll('.progress-bar'); 
+    // navbar 스크롤 작동 애니메이션
 
-    for(let i=0;i<progress.length;i++){
-        let progressWidth = progress[i].getAttribute('data-num'); 
-        progress[i].querySelector('.progress').style.width = progressWidth + "%"
+    let navbar = document.querySelector('.navbar'); 
+    let navbarLink = document.querySelectorAll('.navbar-menu a.scroll');
+    let navbarCurrent;
+
+    // active 효과 추가
+    function activateScroll(elem){
+        elem.classList.add('active'); 
+        navbarCurrent = elem; 
     }
 
+    // navbar a링크 클릭시 a.active 추가 및 해당 메뉴Top으로 이동
+    function navbarLinkClick(e) { 
+        e.preventDefault();
+        
+        const navbarHandler = e.target; 
+        if(navbarCurrent) navbarCurrent.classList.remove('active'); 
+
+        activateScroll(navbarHandler); 
+        let targetId = navbarHandler.getAttribute('href'); 
+
+        window.scrollTo({
+            top: document.querySelector(targetId).offsetTop, 
+            behavior: "smooth"
+        });         
+    }
+    
+    // navbar메뉴 a링크 클릭 이벤트적용
+    for(let i=0;i<navbarLink.length;i++){
+        activateScroll(navbarLink[0]); 
+        navbarLink[i].addEventListener('click', navbarLinkClick);
+    }
+
+    if( window.pageYOffset > 0 ) navbar.classList.add('show'); 
+
+    function scrollEvent() { 
+        
+        if ( window.pageYOffset > 0 ) { 
+            navbar.classList.add('show')
+        } else if ( window.pageYOffset == 0 ) { 
+            navbar.classList.remove('show')
+        }
+
+        for(let i=0;i<navbarLink.length;i++){
+            let targetId = navbarLink[i].getAttribute('href'); 
+            let currentPageId = document.querySelector(targetId);        
+            
+            if(window.pageYOffset >= currentPageId.offsetTop) { 
+                if(navbarCurrent) navbarCurrent.classList.remove('active'); 
+                activateScroll(navbarLink[i]); 
+            }
+        }
+    }
+
+    // 스크롤 이벤트 추가
+    window.addEventListener('scroll', scrollEvent); 
 }
